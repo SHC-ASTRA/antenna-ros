7843f60490e0b9783fc6ff179ee386b6943cbbfa
ryleu <69326171+ryleu@users.noreply.github.com>
2025-10-21 11:49:55 -0500

    update astra_msgs

--- overlay.nix
+++ overlay.nix
@@ -1,5 +1,5 @@
 final: prev:
 {
-  antenna-pkg = final.callPackage ././install/antenna_pkg/share/antenna_pkg/package.nix {};
-  ros2-interfaces-pkg = final.callPackage ././install/ros2_interfaces_pkg/share/ros2_interfaces_pkg/package.nix {};
+  antenna-pkg = final.callPackage ././src/antenna_pkg/package.nix {};
+  astra-msgs = final.callPackage ././src/astra_msgs/package.nix {};
 }
